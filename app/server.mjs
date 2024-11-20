24926e47973c0e8841fe2a9d679175232550f079
py1u.dev <py1u.dev@gmail.com>
2024-11-19 23:32:02 -0800

    update: control flow example

--- app/server.mjs
+++ app/server.mjs
@@ -1,16 +1,23 @@
-function final(someInput, callback) {
-  callback(`${someInput} and terminated by executing callback `);
+function getSong() {
+  let _song = "";
+  let i = 100;
+  for (i; i > 0; i -= 1) {
+    _song += `${i} beers on the wall, you take one down and pass it around, ${
+      i - 1
+    } bottles of beer on the wall\n`;
+    if (i === 1) {
+      _song += "Hey let's get some more beer";
+    }
+  }
+
+  return _song;
 }
 
-function middleware(someInput, callback) {
-  return final(`${someInput} touched by middleware `, callback);
+function singSong(_song) {
+  if (!_song) throw new Error("song is '' empty, FEED ME A SONG!");
+  console.log(_song);
 }
 
-function initiate() {
-  const someInput = "hello this is a function ";
-  middleware(someInput, function (result) {
-    console.log(result);
-  });
-}
-
-initiate();
+const song = getSong();
+// this will work
+singSong(song);
